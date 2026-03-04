90add3c158db0ddb74ce270ed8a71efb2617e000
Patra <anugrahpatra.24@gmail.com>
2026-03-04 13:30:19 +0700

    feat: push changePasswordRequest

---
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ChangePasswordRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'current_password' => 'required|string',
+            'password' => 'required|string|min:8|confirmed',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'current_password.required' => 'Password saat ini wajib diisi.',
+            'password.min' => 'Password baru minimal 8 karakter.',
+            'password.confirmed' => 'Konfirmasi password tidak sesuai.',
+        ];
+    }
+}
